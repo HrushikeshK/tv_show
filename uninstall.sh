68ebd97f92d926b5847cbf21697d8dd9417ecda5
Hrushikesh <hru123.kakade@gmail.com>
2016-03-19 17:19:27 +0530

    Added uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+Dir=$(sed -n 1p ~/.TVshowLog/.location.log)		# TV_show directory
+Log_dir="$HOME/.TVshowLog" 		# Log Directory
+
+sudo rm -r "$Dir"
+sudo rm -r "$Log_dir"
+
+## Remove aliases, if made any ##
