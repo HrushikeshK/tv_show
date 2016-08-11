05290a00f3c9d589ff83476c9ae10462020a8854
hrushikeshk <hru123.kakade@gmail.com>
2016-08-11 14:37:06 +0530

    Added code to remove link (if any)

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,13 @@ sudo rm -r "$Dir"
 sudo rm -r "$Log_dir"
 
 ## Remove aliases, if made any ##
+
+echo "Removing link (if any)... "
+if [ -f /usr/bin/tv ];then
+	echo "Enter password to remove link" 
+	sudo rm /usr/bin/tv
+	echo "Link removed..."
+else 
+	echo "No link found"
+fi
+
