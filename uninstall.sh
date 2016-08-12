577a5c34621c6d88181ec2daf1127dc97f90efd1
hrushikeshk <hru123.kakade@gmail.com>
2016-08-12 18:55:59 +0530

    Added code to remove soft-link from /usr/bin (if any)

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,17 @@
+#!/bin/bash
+
 Dir=$(sed -n 1p "$HOME/.TVshowLog/.location.log")		# TV_show directory
 Log_dir="$HOME/.TVshowLog" 		# Log Directory
-
-sudo rm -r "$Dir"
-sudo rm -r "$Log_dir"
 
 ## Remove link, if any ##
 
 echo "Removing link (if any)... "
-if [ -f /usr/bin/tv ];then
-	echo "Enter password to remove link" 
-	sudo rm /usr/bin/tv
-	echo "Link removed..."
-else 
-	echo "No link found"
-fi
+if [ -f "$Log_dir/.install" ]; then
+	link_name=`sed -n 2p "$Log_dir/.install"`		# Path where link is created	
+	sudo rm "/usr/bin/$link_name"
+	echo "link removed..."
+fi 
 
+echo "Removing logs..."
+sudo rm -r "$Log_dir"
+sudo rm -r "$Dir"
