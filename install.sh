3c49f574cdf0dc8e14f940dcb4e5d288773e80cc
hrushikeshk <hru123.kakade@gmail.com>
2016-08-12 18:57:59 +0530

    Removed functions to create link and added in source.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
+source ./source.sh
 
-readonly script_name="tv.sh"		# Get name of the script
-readonly relative_location=`dirname "$0"`		# To get relative path of the script, dirname command deletes last entry of the path
-readonly script_location="`( cd "$relative_location" && pwd)`"	# To get absolute path of the script
+main() {
 
-echo "This will create a link in /usr/bin for the script (Default name: tv)"
-read -p "Do you want to continue? [Y/n]" response
-name="tv"
+	if [ ! -d $HOME/.TVshowLog ]; then			# If the Log directory does not exist, then create one.
+		mkdir "$HOME/.TVshowLog"					# Needed for first time only, mostly
+	fi
 
-if [ -z $response ] || [ $response = 'y' ] || [ $response = 'Y' ]; then
-	while true; do
-		if [ ! -f "/usr/bin/$name" ]; then
-			sudo ln -s "$script_location/$script_name" "/usr/bin/$name"
-			echo "Link created... Close the terminal and start again and type $name to run the script"
-			break
-		else
-			echo "There is already a program with name $name"
-			echo "Enter another name you would like to use for this script or press <q> to quit"
-			read name
-			if [ $name = 'q' ]; then 
-				echo "Another way is to create an alias"
-				break
-			fi
-		fi
-	done
-else
-	echo "Another way is to create an alias"
-fi
+	if [ ! -f $HOME/.TVshowLog/.help.txt ]; then
+		cp "$script_location/tvshow_help.txt" "$HOME/.TVshowLog/.help.txt"		# Copy the help file in Log folder
+	fi
+
+	echo "$script_location" > "$tvShowLog/.install"		# 1st line in .install file
+
+	createLog		# Function call from source.sh file
+	createLink		# Function call from source.sh file
+}
+
+# main function call
+
+main		
